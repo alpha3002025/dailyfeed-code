4548a24e2a7ebfaa5552b12c654e6c149ab8e800
alpha3002025 <alpha300uk@gmail.com>
2025-09-29 15:32:08 +0900

    feat: 맞팔로우 체크 로직 버그 수정

---
+++ src/main/java/click/dailyfeed/code/domain/member/follow/predicate/FollowingPredicate.java
@@ -0,0 +1,10 @@
+package click.dailyfeed.code.domain.member.follow.predicate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum FollowingPredicate {
+    FOLLOWING, NOT_FOLLOWING;
+}
