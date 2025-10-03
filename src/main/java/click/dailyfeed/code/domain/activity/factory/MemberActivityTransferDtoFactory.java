12e211200367ca96d6cfd15da32dae7c2886cbf2
alpha3002025 <alpha300uk@gmail.com>
2025-10-03 19:08:44 +0900

    feat: 멤버 활동 기록 개편

--- src/main/java/click/dailyfeed/code/domain/activity/factory/MemberActivityTransferDtoFactory.java
+++ src/main/java/click/dailyfeed/code/domain/activity/factory/MemberActivityTransferDtoFactory.java
@@ -15,10 +15,30 @@ public class MemberActivityTransferDtoFactory {
                 .build();
     }
 
+    public static MemberActivityTransportDto.MemberActivityEvent newCommentMemberActivityTransportDto(Long memberId, Long postId, Long commentId, MemberActivityType activityType, LocalDateTime now) {
+        return MemberActivityTransportDto.MemberActivityEvent.builder()
+                .memberId(memberId)
+                .postId(postId)
+                .commentId(commentId)
+                .memberActivityType(activityType)
+                .createdAt(now)
+                .build();
+    }
+
     public static MemberActivityTransportDto.MemberActivityEvent newPostLikeMemberActivityTransportDto(Long memberId, Long postId, MemberActivityType activityType, LocalDateTime now) {
         return MemberActivityTransportDto.MemberActivityEvent.builder()
                 .memberId(memberId)
                 .postId(postId)
+                .memberActivityType(activityType)
+                .createdAt(now)
+                .build();
+    }
+
+    public static MemberActivityTransportDto.MemberActivityEvent newCommentLikeMemberActivityTransportDto(Long memberId, Long postId, Long commentId, MemberActivityType activityType, LocalDateTime now) {
+        return MemberActivityTransportDto.MemberActivityEvent.builder()
+                .memberId(memberId)
+                .postId(postId)
+                .commentId(commentId)
                 .memberActivityType(activityType)
                 .createdAt(now)
                 .build();
