fd10d7accbf40027caaff78186a34f6f7ff726e3
alpha3002025 <alpha300uk@gmail.com>
2025-10-22 12:09:06 +0900

    feat: deadletter 기능 개발 및 정리

--- src/main/java/click/dailyfeed/code/global/kafka/code/KafkaExceptionCode.java
+++ src/main/java/click/dailyfeed/code/global/kafka/code/KafkaExceptionCode.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum KafkaExceptionCode {
     KAFKA_NETWORK_ERROR("잠시 후에 다시 시도해주세요", "KAFKA_NETWORK_ERROR", 500),
-    KAFKA_DLQ_REDIS_NETWORK_ERROR("잠시 후에 다시 시도해주세요", "KAFKA_DLQ_REDIS_NETWORK_ERROR", 500),
     KAFKA_KEY_CREATION_ERROR("잠시 후에 다시 시도해주세요", "KAFKA_KEY_CREATION_ERROR", 500);
 
     private final String message;
