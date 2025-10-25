87f837ccae229675d127b52519e857407ee0d05d
alpha3002025 <alpha300uk@gmail.com>
2025-10-25 16:55:17 +0900

    feat: 블랙리스트,RefreshToken 기능에 Redis 레이어를 통해 최적화

--- src/main/java/click/dailyfeed/code/global/cache/RedisKeyPrefix.java
+++ src/main/java/click/dailyfeed/code/global/cache/RedisKeyPrefix.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RedisKeyPrefix {
+    MEMBER_AUTHENTICATION_BLACKLIST("member:authentication:blacklist:"),
+    MEMBER_AUTHENTICATION_REVOKED_REFRESH("member:authentication:revoked_refresh:"),
     MEMBER_ACTIVITY_KAFKA_EVENT("member_activity:kafka_event:"),
     MEMBER_ACTIVITY_KAFKA_LISTENER("member_activity:kafka_listener:"),
     MEMBER_ACTIVITY_KAFKA_KEY("member_activity:kafka_key:"),
